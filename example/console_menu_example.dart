6a1fd585f4779bee9cd319ca07501b06145df4db
Jim Hessin <jhessin@gmail.com>
2021-10-03 21:06:33 -0600

    Initial Commit

---
+++ example/console_menu_example.dart
@@ -0,0 +1,6 @@
+import 'package:console_menu/console_menu.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/console_menu.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library console_menu;
+
+export 'src/console_menu_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
